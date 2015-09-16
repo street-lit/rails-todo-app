03db036d378ebadec3c756ff5b8fc39c7ca90bb8
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-09-16 18:10:48 -0500

    Add 'create' to Tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,13 +10,73 @@ class TasksController < ApplicationController
     render json: task.to_json, status: 200
   end
 
+  def show
+    if Task.exists?(params[:id])
+      task = Task.find(params[:id])
+      render json: task.to_json, status: 200
+    else
+      render json: { error_msg: 'Record Not Found!', id: params[:id] }.to_json, status: 404
+    end
+  end
+
   def create
-
+    if params[:name].nil? || params[:name].empty?
+      err_msg = "The 'body' parameter was empty or not found"
+      render json: { error_msg: err_msg }.to_json, status: 422
+    else
+      task = Task.new
+      task.name = params[:name]
+      task.save
+      render json: task.to_json, status: 201
+    end
   end
 
-  def destroy
-
-  end
 end
 
 # puts "PARAMS => #{params.inspect}"
+
+# Sending a POST request to http://localhost:3000/todos with the params of { "body": "Finish Homework" }
+# I should see my todo returned to me as json (Using Postman)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get('todos', { to: 'tasks#index' })
-  get('todos/new', { to: 'tasks#new' })
-  get('/todos/:id', { to: 'tasks#show' })
+  get('/todos',        { to: 'tasks#index' })
+  get('/todos/new',    { to: 'tasks#new' })
+  get('/todos/:id',    { to: 'tasks#show' })
+  post('/todos',       { to: 'tasks#create' })
+  delete('/todos/:id', { to: 'tasks#destroy' })
+  put('/todos/:id',    { to: 'tasks#create' })
 end
