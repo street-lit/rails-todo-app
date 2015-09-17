620d2f8a13f72bec464655b98f935640181760ee
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-09-17 11:06:36 -0500

    Create seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+words = %w(walk make dog clean run the kitchen groceries food pay insurance bills homework do the study run bank car dishes dust)
+
+100.times do |n|
+  task = 5.times.map { words.sample }.join(' ')
+  Task.create({ name: task })
+end
