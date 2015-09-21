63313dc246f760c9998518ea94d7e67b2ecf0177
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-09-21 16:00:11 -0500

    Update database to work with html/css

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150916203732) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string   "name",       limit: 300,                 null: false
     t.boolean  "complete",               default: false
